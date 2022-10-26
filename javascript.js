dd66ca0cd0a71e0b1a0511cbbc1bc00f2f4f9b9f
Thompalo <thomas@epalo.no>
2022-10-26 12:12:09 +0200

    Finish computers random choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,20 @@
-console.log("World!");
+// Generate a random choice for the computer
+function getComputerChoice() {
+    const computerChoiceArray = ["Rock", "Paper", "Scissors"];
+    const computerChoice = computerChoiceArray[Math.floor(Math.random() * computerChoiceArray.length)];
+    console.log(computerChoice);
+    return computerChoice;
+}
+getComputerChoice();
+
+// Play a round
+
+function playOneRound(playerSelection, computerSelection) {
+
+}
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+
+
+// Display winner and loser
+// Make the game five rounds long
